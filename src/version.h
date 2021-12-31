499084890fe9e6675f375d197fce4479b7c94bbe
Scott Seligman <scott.seligman@gmail.com>
2021-12-30 17:04:04 -0800

    Release v0.0.4

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 
 #define VERSION_1       0
 #define VERSION_2       0
-#define VERSION_3       3
-#define VERSION_TEXT    "0.0.3"
+#define VERSION_3       4
+#define VERSION_TEXT    "0.0.4"
 #define VERSION_TEXT_T  _T(VERSION_TEXT)
 #define VERSION_YEAR    "2021"
 #define VERSION_YEAR_T  _T(VERSION_YEAR)
