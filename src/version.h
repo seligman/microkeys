d3311f51f0bf79d36885d4ed272ef3b05ab0212f
Scott Seligman <scott.seligman@gmail.com>
2021-12-31 17:52:02 -0800

    Release v0.1.0

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #define VERSION_1       0
-#define VERSION_2       0
-#define VERSION_3       5
-#define VERSION_TEXT    "0.0.5"
+#define VERSION_2       1
+#define VERSION_3       0
+#define VERSION_TEXT    "0.1.0"
 #define VERSION_TEXT_T  _T(VERSION_TEXT)
 #define VERSION_YEAR    "2021"
 #define VERSION_YEAR_T  _T(VERSION_YEAR)
