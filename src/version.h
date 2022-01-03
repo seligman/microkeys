239a8376ab398072035e6caca19a2e8dd254588a
Scott Seligman <scott.seligman@gmail.com>
2022-01-03 12:55:06 -0800

    Release v0.2.2

--- src/version.h
+++ src/version.h
@@ -2,8 +2,8 @@
 
 #define VERSION_1       0
 #define VERSION_2       2
-#define VERSION_3       1
-#define VERSION_TEXT    "0.2.1"
+#define VERSION_3       2
+#define VERSION_TEXT    "0.2.2"
 #define VERSION_TEXT_T  _T(VERSION_TEXT)
 #define VERSION_YEAR    "2022"
 #define VERSION_YEAR_T  _T(VERSION_YEAR)
